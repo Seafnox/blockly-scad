10f70b02bd9345abeb84c5ee41e37e55a955f140
Kirill Warp <kirill9990@gmail.com>
2022-09-22 19:57:05 +0300

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import Split from 'react-split';
+import { Blockly } from './components/Blockly';
+import { Console } from './components/Console';
+import { Editor } from './components/Editor';
+import { Header } from './components/Header';
 import './App.css';
+import { Preview } from './components/Preview';
+import { IdeStateService } from './services/ideStateService';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component<App, App> {
+  ideService = IdeStateService.instance();
+
+  componentDidMount() {
+    window.addEventListener('resize', () => IdeStateService.instance().resize.emit(), false);
+    this.ideService.resize.emit();
+  }
+
+  onVerticalSplitDrag() {
+    this.ideService.resize.emit();
+  }
+
+  onHorizontalSplitDrag() {
+    this.ideService.resize.emit();
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Header></Header>
+        <Split direction="vertical" sizes={[50, 50]} onDrag={this.onVerticalSplitDrag}>
+          <Split direction="horizontal" sizes={[75, 25]} onDrag={this.onHorizontalSplitDrag}>
+            <Blockly/>
+            <Console/>
+          </Split>
+          <Split direction="horizontal" sizes={[50, 50]}>
+            <Preview/>
+            <Editor/>
+          </Split>
+        </Split>
+      </Fragment>
+    );
+  }
 }
-
-export default App;
